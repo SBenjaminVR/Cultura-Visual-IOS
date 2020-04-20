b70b6b3491159240d134e0bab427a62e82adca8e
Gibran Gzz <hector.gibran@hotmail.com>
2020-04-20 16:04:31 -0500

    Adds tap gesture for answer pictures, fixes modes for different types of answers

--- Cultura Visual y Sonora/Cultura Visual y Sonora/CreacionCuestionario.swift
+++ Cultura Visual y Sonora/Cultura Visual y Sonora/CreacionCuestionario.swift
@@ -37,13 +37,24 @@ class CreacionCuestionario: UIViewController, UIImagePickerControllerDelegate, U
     
     @IBOutlet weak var btnCrearCuestionario: UIButton! //Boton para crear el cuestionario
     
+    //Outlets para taps
+    @IBOutlet var tapFotoPreg: UITapGestureRecognizer!
+    @IBOutlet var tapFotoR1: UITapGestureRecognizer!
+    @IBOutlet var tapFotoR2: UITapGestureRecognizer!
+    @IBOutlet var tapFotoR3: UITapGestureRecognizer!
+    @IBOutlet var tapFotoR4: UITapGestureRecognizer!
+    
+    var selectedTap:Int!
+    
     var delegado : protocoloAgregaPreg!
     var listaPreguntas : [Pregunta] = [Pregunta]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imgR1.isUserInteractionEnabled = false
+        imgR2.isUserInteractionEnabled = false
+        imgR3.isUserInteractionEnabled = false
+        imgR4.isUserInteractionEnabled = false
     }
     
     // MARK: - Creacion Preguntas
@@ -97,18 +108,29 @@ class CreacionCuestionario: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     // MARK: - ImagePickerDelegate
-    
-    @IBAction func agregarFoto(_ sender: Any) {
+    @IBAction func agregarFoto(_ sender: UITapGestureRecognizer) {
         let picker = UIImagePickerController()
         picker.delegate = self
         picker.sourceType = .photoLibrary
+        
+        if sender == tapFotoPreg {
+            selectedTap = 0
+        } else if sender == tapFotoR1 {
+            selectedTap = 1
+        } else if sender == tapFotoR2 {
+            selectedTap = 2
+        } else if sender == tapFotoR3 {
+            selectedTap = 3
+        } else if sender == tapFotoR4 {
+            selectedTap = 4
+        }
         
         present(picker, animated: true, completion: nil)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let foto = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-        imgPreg.image = foto
+        choosePhoto(foto: foto)
         dismiss(animated: true, completion: nil)
     }
     
@@ -116,7 +138,64 @@ class CreacionCuestionario: UIViewController, UIImagePickerControllerDelegate, U
         dismiss(animated: true, completion: nil)
     }
     
+    func choosePhoto(foto: UIImage) {
+        if selectedTap == 0 {
+            imgPreg.image = foto
+        } else if selectedTap == 1 {
+            imgR1.image = foto
+        } else if selectedTap == 2 {
+            imgR2.image = foto
+        } else if selectedTap == 3 {
+            imgR3.image = foto
+        } else if selectedTap == 4 {
+            imgR4.image = foto
+        }
+    }
     
+    
+    @IBAction func changeQuestionType(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            tfRespuesta1.text = ""
+            tfRespuesta2.text = ""
+            
+            tfRespuesta3.isEnabled = true
+            tfRespuesta4.isEnabled = true
+        } else {
+            tipoRespuestas.selectedSegmentIndex = 0
+            
+            tfRespuesta1.text = "Verdadero"
+            tfRespuesta2.text = "Falso"
+            
+            tfRespuesta3.isEnabled = false
+            tfRespuesta4.isEnabled = false
+            
+        }
+    }
+    
+    @IBAction func changeAnswerType(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0  {
+            imgR1.isUserInteractionEnabled = false
+            imgR2.isUserInteractionEnabled = false
+            imgR3.isUserInteractionEnabled = false
+            imgR4.isUserInteractionEnabled = false
+            
+            tfRespuesta1.isEnabled = true
+            tfRespuesta2.isEnabled = true
+            tfRespuesta3.isEnabled = true
+            tfRespuesta4.isEnabled = true
+        } else {
+            tipoPreguntas.selectedSegmentIndex = 0
+            imgR1.isUserInteractionEnabled = true
+            imgR2.isUserInteractionEnabled = true
+            imgR3.isUserInteractionEnabled = true
+            imgR4.isUserInteractionEnabled = true
+            
+            tfRespuesta1.isEnabled = false
+            tfRespuesta2.isEnabled = false
+            tfRespuesta3.isEnabled = false
+            tfRespuesta4.isEnabled = false
+        }
+    }
     
     // MARK: - Navigation
 
