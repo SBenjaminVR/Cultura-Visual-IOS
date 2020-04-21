34c8d0186fc8486242dccf4b86587ff7a7d63ff0
David Cantú <32000336+codyvi@users.noreply.github.com>
2020-04-20 20:36:01 -0500

    ScrollView for ResponderCuestionario works!
    ScrollView now works but the functionality isn't yet done, needs to be done ASAP for this first revision.

---
+++ Cultura Visual y Sonora/Cultura Visual y Sonora/Slide.swift
@@ -0,0 +1,32 @@
+//
+//  Slide.swift
+//  Cultura Visual y Sonora
+//
+//  Created by user169165 on 4/20/20.
+//  Copyright © 2020 ITESM. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+    
+    // MARK: - Variables
+    
+    @IBOutlet weak var lblNumeroPregunta: UILabel!
+    @IBOutlet weak var imgPregunta: UIImageView!
+    @IBOutlet weak var btnRespuestaTexto1: UIButton!
+    @IBOutlet weak var btnRespuestaTexto2: UIButton!
+    @IBOutlet weak var btnRespuestaTexto3: UIButton!
+    @IBOutlet weak var btnRespuestaTexto4: UIButton!
+
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
