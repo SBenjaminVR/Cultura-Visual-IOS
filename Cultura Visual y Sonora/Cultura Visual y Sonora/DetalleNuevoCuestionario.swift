1c62723bad0520cbf7bfeb152247929260609b08
David Cantú <32000336+codyvi@users.noreply.github.com>
2020-05-04 18:50:15 -0700

    Updated DetalleNuevoCuestionario, has a problem

--- Cultura Visual y Sonora/Cultura Visual y Sonora/DetalleNuevoCuestionario.swift
+++ Cultura Visual y Sonora/Cultura Visual y Sonora/DetalleNuevoCuestionario.swift
@@ -14,9 +14,12 @@ class DetalleNuevoCuestionario: UIViewController, protocoloAgregaPreg {
     
     @IBOutlet weak var tfNombreCuest: UITextField!
     @IBOutlet weak var tfTiempoCuest: UITextField!
+    
+    
 
     var listaPreguntas:[Pregunta] = [Pregunta]()
     var cantPreg : Int = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,19 +57,16 @@ class DetalleNuevoCuestionario: UIViewController, protocoloAgregaPreg {
         for preg in cuest.preguntas {
             guardarPregunta(cuest: cuest, preg: preg, i: i)
             i += 1
-            
-            for j in 0...3 {
-                guardarImagen(image: preg.imagenes[j], cuest: cuest, i: i, tipo: 2, resp: j)
-            }
         }
     }
     
     func guardarPregunta(cuest: Cuestionario, preg: Pregunta, i: Int) {
         let cuestRef = Firestore.firestore().collection("Cuestionarios").document("\(cuest.nombre)")
         
+      
         cuestRef.updateData([
             "preguntas": FieldValue.arrayUnion([
-                [ "pregunta \(i)":
+
                     ["descripcion": preg.descripcion,
                      "categoria": preg.categoria,
                      "respuestas": [
@@ -79,112 +79,19 @@ class DetalleNuevoCuestionario: UIViewController, protocoloAgregaPreg {
                      "imagenPreg": "",
                      "tipoResp": preg.tipoRespuestas
                     ]
-                ]
+                
             ])
         ])
         
         guardarImagen(image: preg.imgPregunta, cuest: cuest, i: i, tipo: 1, resp: 4)
+        guardarImagen(image: preg.imagenes[0], cuest: cuest, i: i, tipo: 0, resp: 0)
+        guardarImagen(image: preg.imagenes[1], cuest: cuest, i: i, tipo: 0, resp: 1)
+        guardarImagen(image: preg.imagenes[2], cuest: cuest, i: i, tipo: 0, resp: 2)
+        guardarImagen(image: preg.imagenes[3], cuest: cuest, i: i, tipo: 0, resp: 3)
     }
-    //guardarImagenRespuestas(image: preg.imagenes[0], cuest: cuest, i: i, tipo: 2, resp: 0)
     
-    func guardarImagenRespuestas(image: UIImage, cuest: Cuestionario, i: Int, tipo: Int, resp: Int) -> String {
-        var imgUrlString = ""
-        print("1")
-        guard let data = image.jpegData(compressionQuality: 1.0) else {
-            let alerta = UIAlertController(title: "Error", message: "Algo salio mal 1", preferredStyle: .alert)
-            let accion = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
-            
-            alerta.addAction(accion)
-            
-            self.present(alerta, animated: true, completion: nil)
-            return ""
-        }
-        
-        var imgName = "\(cuest.nombre)Preg\(i)Resp\(resp).jpeg"
-        
-        let imageReference = Storage.storage().reference(withPath: "images/\(imgName)")
-        
-        print("2")
-        imageReference.putData(data, metadata:nil) { (metadata, error) in
-            if let error = error {
-                let alerta = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
-                let accion = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
-                
-                alerta.addAction(accion)
-                
-                self.present(alerta, animated: true, completion: nil)
-                return
-            }
-            
-            print("3")
-            imageReference.downloadURL(completion: { (url, error) in
-                if let error = error {
-                    let alerta = UIAlertController(title: "Error", message: "Algo salio mal 3", preferredStyle: .alert)
-                    let accion = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
-                    
-                    alerta.addAction(accion)
-                    
-                    self.present(alerta, animated: true, completion: nil)
-                    return
-                }
-                
-                print("4")
-                guard let url = url else {
-                    let alerta = UIAlertController(title: "Error", message: "Algo salio mal 4", preferredStyle: .alert)
-                    let accion = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
-                    
-                    alerta.addAction(accion)
-                    
-                    self.present(alerta, animated: true, completion: nil)
-                    return
-                }
-                
-                print("ENTREEEEEEEEEEEE")
-                
-                imgUrlString =  url.absoluteString
-                /*print(urlString)
-                
-                print("5")
-                let dataReference = Firestore.firestore().collection("Cuestionarios").document("\(cuest.nombre)")
-                
-                if tipo == 1 {
-                    dataReference.updateData([
-                        "imagenPreg": urlString
-                    ]) { (error) in
-                        let alerta = UIAlertController(title: "Error", message: "Algo salio mal 5", preferredStyle: .alert)
-                        let accion = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
-                        
-                        alerta.addAction(accion)
-                        
-                        self.present(alerta, animated: true, completion: nil)
-                        return
-                    }
-                } else {
-                    dataReference.updateData([
-                        "preguntas/imagenes": FieldValue.arrayUnion([urlString])
-                    ]) { (error) in
-                        let alerta = UIAlertController(title: "Error", message: "Algo salio mal 5", preferredStyle: .alert)
-                        let accion = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
-                        
-                        alerta.addAction(accion)
-                        
-                        self.present(alerta, animated: true, completion: nil)
-                        return
-                    }
-                }
-                
-                let alerta = UIAlertController(title: "Success", message: "Updated imagen pregunta", preferredStyle: .alert)
-                let accion = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
-                
-                alerta.addAction(accion)
-                
-                self.present(alerta, animated: true, completion: nil)*/
-            })
-        }
-        return imgUrlString
-    }
-
-    func guardarImagen(image: UIImage, cuest: Cuestionario, i: Int, tipo: Int, resp: Int) {
+    
+ func guardarImagen(image: UIImage, cuest: Cuestionario, i: Int, tipo: Int, resp: Int) {
         guard let data = image.jpegData(compressionQuality: 1.0) else {
             let alerta = UIAlertController(title: "Error", message: "Algo salio mal 1", preferredStyle: .alert)
             let accion = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
@@ -226,7 +133,7 @@ class DetalleNuevoCuestionario: UIViewController, protocoloAgregaPreg {
                     return
                 }
                 
-                print("4")
+            
                 guard let url = url else {
                     let alerta = UIAlertController(title: "Error", message: "Algo salio mal 4", preferredStyle: .alert)
                     let accion = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
@@ -238,12 +145,11 @@ class DetalleNuevoCuestionario: UIViewController, protocoloAgregaPreg {
                 }
                 
                 let urlString = url.absoluteString
-                print(urlString)
                 
                 let dataReference = Firestore.firestore().collection("Cuestionarios").document("\(cuest.nombre)")
-        
+                if tipo == 1{
                     dataReference.updateData([
-                        "imagenPreg": urlString
+                        "preguntas.\(i).imagenPreg": urlString
                     ]) { (error) in
                         let alerta = UIAlertController(title: "Error", message: "Algo salio mal 5", preferredStyle: .alert)
                         let accion = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
@@ -253,14 +159,23 @@ class DetalleNuevoCuestionario: UIViewController, protocoloAgregaPreg {
                         self.present(alerta, animated: true, completion: nil)
                         return
                     }
+                }
+                else{
+                    dataReference.updateData([
+                        "preguntas.\(i).imagenes": FieldValue.arrayUnion([urlString])
+                    ]) { (error) in
+                        let alerta = UIAlertController(title: "Error", message: "Algo salio mal 5", preferredStyle: .alert)
+                        let accion = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+                        
+                        alerta.addAction(accion)
+                        
+                        self.present(alerta, animated: true, completion: nil)
+                        return
+                    }
+                }
                 
                 
-                let alerta = UIAlertController(title: "Success", message: "Updated imagen pregunta", preferredStyle: .alert)
-                let accion = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
                 
-                alerta.addAction(accion)
-                
-                self.present(alerta, animated: true, completion: nil)
             })
         }
     }
