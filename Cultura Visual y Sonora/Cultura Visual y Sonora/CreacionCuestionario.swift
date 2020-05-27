253d552f6585689b33d1da03b8e675357f5794bf
Benjamín Valdez <benjamin.vr.1998@gmail.com>
2020-05-27 00:47:19 -0500

    Now the view gets pushed up when there's a keyboard

--- Cultura Visual y Sonora/Cultura Visual y Sonora/CreacionCuestionario.swift
+++ Cultura Visual y Sonora/Cultura Visual y Sonora/CreacionCuestionario.swift
@@ -61,7 +61,26 @@ class CreacionCuestionario: UIViewController, UIImagePickerControllerDelegate, U
         imgR2.isUserInteractionEnabled = false
         imgR3.isUserInteractionEnabled = false
         imgR4.isUserInteractionEnabled = false
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+    
+    
     
     override var shouldAutorotate: Bool {
         return false
